99d3aa6b7972a547fb9357456bbe1853ae84660a
last-las <478942543@qq.com>
2022-03-30 11:08:25 +0800

    add comment for 06heap.rs

--- user/src/bin/06heap.rs
+++ user/src/bin/06heap.rs
@@ -9,6 +9,7 @@ use alloc::vec::Vec;
 use alloc::boxed::Box;
 
 const FRAME_SIZE: usize = 0x1000;
+const USER_HEAP_SIZE: usize = 0x4000;
 
 #[no_mangle]
 fn main() {
@@ -30,8 +31,16 @@ fn allocating_vector() {
 }
 
 fn allocating_large_memory() {
+    /*
+        This function will allocate `USER_HEAP_SIZE` bytes memory.
+
+        However, because of the implementation of `USER_HEAP_ALLOCATOR`,
+        allocating `USER_HEAP_SIZE` bytes at once doesn't guarantee success.
+        So instead it allocates `FRAME_SIZE` at once
+         and allocates `USER_HEAP_SIZE` / `FRAME_SIZE` times.
+    */
     println!("test allocating large memory");
-    let boxes: [Box<[u8; FRAME_SIZE]>; 4] =  [
+    let boxes: [Box<[u8; FRAME_SIZE]>; USER_HEAP_SIZE / FRAME_SIZE] =  [
         Box::new([0; FRAME_SIZE]),
         Box::new([0; FRAME_SIZE]),
         Box::new([0; FRAME_SIZE]),
