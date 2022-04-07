6970e062e623e668c6caccd648a486ac0b5750dd
last-las <478942543@qq.com>
2022-04-07 11:16:33 +0800

    update code structure; implement some interface for filesystem

--- os/src/syscall/proc/do_fork.rs
+++ os/src/syscall/proc/do_fork.rs
@@ -4,6 +4,7 @@ use crate::processor::clone_cur_task_in_this_hart;
 use share::syscall::error::{SysError, EAGAIN};
 use alloc::vec::Vec;
 use spin::Mutex;
+// clone
 
 #[allow(unused_variables)]
 pub fn do_fork(flags: u32, stack: usize, ptid_ptr: usize, tls_ptr: usize, ctid_ptr: usize) -> Result<usize, SysError>{
