5b1707394963496dbdf0cf2773e836d13f3394f6
last-las <478942543@qq.com>
2022-04-14 16:16:26 +0800

    update virtio-blk driver with external library: https://github.com/rcore-os/virtio-drivers

--- user/drivers/virtio-blk/src/main.rs
+++ user/drivers/virtio-blk/src/main.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+mod virtio_driver;
+
 #[macro_use]
 extern crate user_lib;
 #[macro_use]
 extern crate alloc;
+#[macro_use]
+extern crate bitflags;
+extern crate volatile;
+extern crate log;
 
 #[no_mangle]
 fn main() {
