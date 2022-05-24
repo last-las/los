a843dcc0c17e175f45b54026252e2f0b0bd2cd51
p0lar1s <p0lar1s@qq.com>
2022-05-24 19:16:10 +0800

    test

--- user/lib/src/heap.rs
+++ user/lib/src/heap.rs
@@ -1,14 +1,18 @@
-use buddy_system_allocator::LockedHeap;
+use core::alloc::Layout;
+use buddy_allocator::{Heap, LockedHeap};
 use crate::syscall::{brk, getpid};
 
-const USER_HEAP_SIZE: usize = 0x80_000;
+// change from 0x80_000 to 0x2000
+const USER_HEAP_SIZE: usize = 0x2000;
 
 // TODO-FUTURE: The size of heap should be dynamic rather than fix size `USER_HEAP_ALLOCATOR`
 #[global_allocator]
 static USER_HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub fn init_heap() {
+    // get current brk
     let cur_pos = brk(None).unwrap();
+    // adjust to new brk, with allocating PhysPage operation
     brk(Some(cur_pos + USER_HEAP_SIZE)).unwrap();
     unsafe {
         USER_HEAP_ALLOCATOR.lock().init(cur_pos, USER_HEAP_SIZE);
